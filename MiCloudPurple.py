7b0160c7274c71088bb89914a8a7cfddba631839
David Burel <david.burel@burelovi.eu>
2025-07-12 08:06:26 +0200

    Refactor project structure: remove unused pylint configuration, add Ruff for linting, and update requirements files

---
+++ MiCloudPurple.py
@@ -0,0 +1,16 @@
+"""
+_summary_
+"""
+
+# Standard library imports
+
+# Third-party imports
+
+# First-party imports
+
+# Local application imports
+
+# This file is part of MiCloudPurple, a project for managing and automating tasks in
+# the Microsoft Cloud ecosystem.
+if __name__ == '__main__':
+    pass
